4a8b2912d05f23602fcb2074d9b5f29d8a0e71c0
ayushamleh <ayushamlesh@gmail.com>
2022-09-19 09:29:06 +0530

    in  file express.js installed express and commented text

--- npm/express.js
+++ npm/express.js
@@ -1,1 +1,20 @@
 // provides a robust set of features to develop web and mobile applications.
+// Following are some of the core features of Express framework −
+// Allows to set up middlewares to respond to HTTP Requests.
+// Defines a routing table which is used to perform different actions based on HTTP Method and URL.
+// Allows to dynamically render HTML Pages based on passing arguments to templates.
+
+//npm install express --save
+
+/*
+extra gyaan 
+The above command saves the installation locally in the node_modules directory and creates a directory express inside node_modules. You should install the following important modules along with express −
+
+    body-parser − This is a node.js middleware for handling JSON, Raw, Text and URL encoded form data.
+
+    cookie-parser − Parse Cookie header and populate req.cookies with an object keyed by the cookie names.
+
+    multer − This is a node.js middleware for handling multipart/form-data.
+
+*/
+
